5272ac77e1a4ef7e77bcff6667cba44653f65ca2
adikumar1 <56322335+adikumar1@users.noreply.github.com>
2020-10-31 17:16:47 +0530

    Update c2.cpp

--- c2.cpp
+++ c2.cpp
@@ -1,21 +1,13 @@
-void add(Tnode* root, string b , int val)
+int query(Tnode* root, string b)
 {
-
    Tnode* temp=root;
-
    for(int i=0;i<sz(b);i++)
    {
-
-    int d=(b[i]-'a');
-  
-     if(temp->children[d]==NULL){
-    temp->children[d]= new Tnode(); 
-     }
-
-     temp=(temp->children[d]);
-
-     temp->data=max(temp->data,val);
-
+     
+    if(temp->children[(b[i]-'a')]==NULL)
+    return -1;
+     temp=temp->children[(b[i]-'a')];
    }
 
+   return temp->data;
 }
